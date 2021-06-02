e010d925563ffdfd9bfc91c7dec2c842f0512c4f
threeteck <threeteck@users.noreply.github.com>
2021-06-03 00:33:10 +0300

    fixed issue when users would not have role claims(only for new accounts) + fixed some front issues

--- SupportChat/ClaimsPrincipalExtensions.cs
+++ SupportChat/ClaimsPrincipalExtensions.cs
@@ -5,6 +5,6 @@ namespace SupportChat
 {
     public static class ClaimsPrincipalExtensions
     {
-        public static bool HasSupportClaim(this ClaimsPrincipal user) => user.HasClaim("support", "support");
+        public static bool HasSupportClaim(this ClaimsPrincipal user) => user.IsInRole("support");
     }
 }
